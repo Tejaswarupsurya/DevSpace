9f05463e0dcd7853cca5c350aab47d802fdbefcc
Tejaswarupsurya <tejaswarupsurya@gmail.com>
2026-01-03 23:13:42 +0530

    Implemented CRUD api calls using Next.js file-based routing and dynamic routing

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: process.cwd(), // Explicitly set the correct project root
+  },
 };
 
 export default nextConfig;
